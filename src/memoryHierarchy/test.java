56a05c9df3023a3572676731086cf924879812e3
Youssuf Radi <y.sameh1995@gmail.com>
2016-11-21 02:26:16 +0200

    Test is working fine

--- src/memoryHierarchy/test.java
+++ src/memoryHierarchy/test.java
@@ -4,16 +4,20 @@ public class test {
 
 	public static void main(String[] args) {
 		
-		 Cache c = new Cache(8, 2, 1, WritingPolicy.WRITE_THROUGH, 5);
+		 Cache c = new Cache(2, 2, 1, WritingPolicy.WRITE_THROUGH, 5);
+		 Cache c1 = new Cache(4, 2, 1, WritingPolicy.WRITE_THROUGH, 5);
+		 Cache c2 = new Cache(8, 2, 1, WritingPolicy.WRITE_THROUGH, 5);
 //		 System.out.println(c.getLineSize());
 //		 System.out.println(c.getSets().length);
 //		 System.out.println(c.getSets()[0].getBlocks().size());
 		 
-		 MemoryHierarchy main = new MemoryHierarchy( new Cache[] {c}, 50);
+		 MemoryHierarchy main = new MemoryHierarchy( new Cache[] {c, c1 ,c2}, 50);
 //		 System.out.println(main.getCaches().length);
 		 main.write(new Byte("Hi"), 4000);
 //		 System.out.println(main.getMainMemory().getData()[4000].getData());
 		 System.out.println(c.getSets()[0].getBlocks().get(0).getData()[0].getData());
+		 System.out.println(c1.getSets()[0].getBlocks().get(0).getData()[0].getData());
+		 System.out.println(c2.getSets()[0].getBlocks().get(0).getData()[0].getData());
 //		 for(int i=0; i<c.getSets().length;i++){
 //			 if(c.getSets()[i].getBlocks().size() != 0 && c.getSets()[i].getBlocks().get(0).getData() != null && c.getSets()[i].getBlocks().get(0).getData()[0] != null ){
 //			 System.out.println(c.getSets()[i].getBlocks().get(0).getData()[0].getData());
