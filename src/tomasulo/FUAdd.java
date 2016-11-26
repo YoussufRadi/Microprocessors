1fc3bd3efaa6e0811e4aa2e0de1dce7c59c3725f
HazemElAgaty <hazemelagaty@gmail.com>
2016-11-26 17:11:01 +0200

    add package tomasulo

---
+++ src/tomasulo/FUAdd.java
@@ -0,0 +1,12 @@
+package tomasulo;
+
+import java.util.ArrayList;
+
+public class FUAdd  { //extends FunctionalUnit
+	
+//	public FUAdd(int numberOfInstances, int execTime){
+//		this.numberOfInstances = numberOfInstances;
+//		this.execTime = execTime;
+//		unit = new FunctionalUnit[numberOfInstances];
+//	}
+}
